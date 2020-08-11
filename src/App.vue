81db047ff13a31494ac4266db9afddd866c8ea2b
Patrick Altman <paltman@gmail.com>
2020-08-11 13:45:30 -0500

    Copy over the EH skeleton structure for testing

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,21 @@
 
 <script>
   import FixedSkeleton from '@scaife-viewer/skeleton';
+
   import ReaderWidget from '@scaife-viewer/widget-reader';
+  import MetadataWidget from '@scaife-viewer/widget-metadata';
+  import PassageReferenceWidget from '@scaife-viewer/widget-passage-reference';
+  import PassageAncestorsWidget from '@scaife-viewer/widget-passage-ancestors';
+  import PassageSiblingsWidget from '@scaife-viewer/widget-passage-siblings';
+  import PassageChildrenWidget from '@scaife-viewer/widget-passage-children';
+  import LibraryWidget from '@scaife-viewer/widget-library';
+  import TokenAnnotationWidget from '@scaife-viewer/widget-token-annotations';
+  import NamedEntitiesWidget from '@scaife-viewer/widget-named-entities';
+  import DisplayModeWidget from '@scaife-viewer/widget-display-mode';
+  import ScholiaWidget from '@scaife-viewer/widget-scholia';
+  import AudioWidget from '@scaife-viewer/widget-audio';
+  import WordListWidget from '@scaife-viewer/widget-word-list';
+  import NewAlexandriaWidget from '@scaife-viewer/widget-new-alexandria';
   import TextSizeWidget from '@scaife-viewer/widget-text-size';
   import TextWidthWidget from '@scaife-viewer/widget-text-width';
 
@@ -25,10 +39,27 @@
     },
     computed: {
       left() {
-        return [TextSizeWidget];
+        return [
+          LibraryWidget,
+          PassageReferenceWidget,
+          PassageAncestorsWidget,
+          PassageSiblingsWidget,
+          PassageChildrenWidget,
+        ];
       },
       right() {
-        return [TextWidthWidget];
+        return [
+          MetadataWidget,
+          TextSizeWidget,
+          TextWidthWidget,
+          AudioWidget,
+          DisplayModeWidget,
+          NamedEntitiesWidget,
+          TokenAnnotationWidget,
+          WordListWidget,
+          NewAlexandriaWidget,
+          ScholiaWidget,
+        ];
       },
       main() {
         return ReaderWidget;
