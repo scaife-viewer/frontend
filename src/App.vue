a52b461646ac0d62b16772012dc43d7cce22c83a
Jacob Wegner <jwegner@eldarion.com>
2020-10-13 12:29:36 -0500

    fix linting error
    oh the irony!

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,8 @@
 </template>
 
 <style lang="scss">
-  @import url(
-    https://fonts.googleapis.com/css?family=Noto+Serif:400,
-    400i,
-    700,
-    700i&subset=greek,
-    greek-ext,
-    latin-ext
-  );
-  @import url(https://fonts.googleapis.com/css?family=Noto+Sans:400, 700);
+  @import url('https://fonts.googleapis.com/css?family=Noto+Serif:400,400i,700,700i&subset=greek,greek-ext,latin-ext');
+  @import url('https://fonts.googleapis.com/css?family=Noto+Sans:400,700');
 
   html,
   body {
