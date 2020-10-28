778655ae09d3c5162b0cb49a13f809960adf67dd
Jacob Wegner <jwegner@eldarion.com>
2020-10-28 15:23:40 -0500

    make displayNameCallback exportable

--- packages/widget-passage-citation-toc/src/PassageCitationTOCWidget.vue
+++ packages/widget-passage-citation-toc/src/PassageCitationTOCWidget.vue
@@ -74,6 +74,7 @@
       },
     },
   };
+  export { displayNameCallback };
 </script>
 
 <style lang="scss" scoped>
--- packages/widget-passage-citation-toc/src/index.js
+++ packages/widget-passage-citation-toc/src/index.js
@@ -1,3 +1,9 @@
-import PassageCitationTOCWidget from './PassageCitationTOCWidget.vue';
+import PassageCitationTOCWidget, {
+  displayNameCallback,
+} from './PassageCitationTOCWidget.vue';
 
 export default PassageCitationTOCWidget;
+
+// NOTE: exporting `displayNameCallback` allows us to shim the
+// dynamic display name functionality into SV 1
+export { displayNameCallback };
