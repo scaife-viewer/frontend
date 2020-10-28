0eb59f4dc3431234203e4870698de650baed68ad
Jacob Wegner <jwegner@eldarion.com>
2020-10-28 15:12:15 -0500

    allow widget displayName to access root Vue instance

--- packages/skeleton/src/utils.js
+++ packages/skeleton/src/utils.js
@@ -1,6 +1,10 @@
-const displayName = name => {
+const displayName = (name, $root) => {
+  // NOTE: Passing the root instance in allows
+  // callables to access $scaife, $store, etc
+
+  // https://vuejs.org/v2/guide/components-edge-cases.html#Accessing-the-Root-Instance
   const isCallable = typeof name === 'function';
-  return isCallable ? name() : name;
+  return isCallable ? name($root) : name;
 };
 
 export default {
