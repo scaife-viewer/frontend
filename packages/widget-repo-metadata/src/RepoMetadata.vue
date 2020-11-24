4b1149e38033b641d04168e7771ed6d92349d310
Jacob Wegner <jwegner@eldarion.com>
2020-11-24 17:20:24 -0600

    wire up additional links to repo metadata widget

--- packages/widget-repo-metadata/src/RepoMetadata.vue
+++ packages/widget-repo-metadata/src/RepoMetadata.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="repo-metadata-container" v-if="repos">
-    <div class="repo-metadata-row" v-if="repo.metadata.githubUrl">
+    <div class="repo-metadata-row" v-if="gitHubUrl">
       <div class="label">GitHub:</div>
-      <a
-        v-if="repo.metadata.githubUrl"
-        class="value"
-        :href="repo.metadata.githubUrl"
-        target="_blank"
-      >
+      <a v-if="gitHubUrl" class="value" :href="gitHubUrl" target="_blank">
         {{ repo.name }}
       </a>
     </div>
     <div class="repo-metadata-row">
       <div class="label">Release SHA:</div>
-      <div class="value">{{ repo.sha }}</div>
+      <div class="value">
+        <a :href="repoTreeUrl" target="_blank">
+          {{ sha }}
+        </a>
+      </div>
+    </div>
+    <div class="report-issue-link">
+      <a :href="newIssueUrl" target="_blank">
+        Report an issue with this passage
+      </a>
     </div>
   </div>
 </template>
@@ -27,6 +31,27 @@
     computed: {
       repo() {
         return this.repos.slice(0, 1)[0];
+      },
+      gitHubUrl() {
+        return this.repo.metadata.githubUrl;
+      },
+      urn() {
+        return this.$store.getters['scaife/urn'];
+      },
+      sha() {
+        return this.repo.sha.slice(0, 7);
+      },
+      ctsFilePath() {
+        const [textGroup, workPart] = this.urn.work.split('.');
+        return `data/${textGroup}/${workPart}/${this.urn.work}.xml`;
+      },
+      repoTreeUrl() {
+        return `${this.gitHubUrl}/tree/${this.sha}/${this.ctsFilePath}`;
+      },
+      newIssueUrl() {
+        const title = `Report an issue: ${this.urn}`;
+        const body = `[${this.urn.work}.xml](${this.repoTreeUrl})`;
+        return `${this.gitHubUrl}/issues/new?title=${title}&body=${body}`;
       },
     },
     apollo: {
@@ -91,6 +116,9 @@
       }
       flex-flow: row nowrap;
       margin: 0.5em 0;
+    }
+    .report-issue-link {
+      font-size: var(--sv-report-issue-link-font-size, 10px);
     }
   }
 </style>
