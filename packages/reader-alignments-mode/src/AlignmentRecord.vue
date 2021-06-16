ec5248cbfc3e076e0572c94d220b8561244517cd
Jacob Wegner <jacobwegner@gmail.com>
2021-06-16 12:06:39 -0500

    Experiment with colorblind-friendly colors

--- packages/reader-alignments-mode/src/AlignmentRecord.vue
+++ packages/reader-alignments-mode/src/AlignmentRecord.vue
@@ -33,19 +33,23 @@
     }
   }
   // TODO: Think through "groupings" to allow for consistent colors
+  // TODO: Palette for color blindness cf. https://thenode.biologists.com/data-visualization-with-flying-colors/research/
   .a0 {
+    // #0073b2
     background-color: var(
       --sv-reader-token-alignments-mode-alignment-record-a0-color,
       #f00
     );
   }
   .a1 {
+    // #d55d00
     background-color: var(
       --sv-reader-token-alignments-mode-alignment-record-a1-color,
       #0c0
     );
   }
   .a2 {
+    // #019e73
     background-color: var(
       --sv-reader-token-alignments-mode-alignment-record-a2-color,
       #00f
