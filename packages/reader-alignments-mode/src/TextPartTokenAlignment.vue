55d43dc764da76873b64a7b4050dd99ca9861048
Jacob Wegner <jwegner@eldarion.com>
2021-11-15 15:30:15 -0600

    Add Amiri font

--- packages/reader-alignments-mode/src/TextPartTokenAlignment.vue
+++ packages/reader-alignments-mode/src/TextPartTokenAlignment.vue
@@ -116,8 +116,11 @@
     padding-inline-start: 1rem;
     // FIXME: Proper rtl font support, including Amiri note below
     .line {
-      // FIXME: Load Amiri; working for me locally because I have it installed
-      font-family: 'Amiri', 'Noto Sans';
+      font-family: var(
+        --sv-alignments-line-font-family-rtl,
+        'Amiri',
+        'Noto Sans'
+      );
     }
     .text-md {
       font-size: 24px;
