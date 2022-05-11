68abeca5ced120c9977a01f2adc1d2328283df48
Jacob Wegner <jwegner@eldarion.com>
2022-05-11 07:17:37 -0500

    Restore Lookahead 

--- packages/widget-dictionary-entries/src/DictionaryEntries.vue
+++ packages/widget-dictionary-entries/src/DictionaryEntries.vue
@@ -3,9 +3,7 @@
     <LoaderBall v-if="$apollo.queries.entries.loading" />
     <EmptyMessage v-else-if="entries.length === 0" />
     <template v-else>
-      <!-- FIXME: restore -->
       <Lookahead
-        v-if="false"
         :placeholder="placeholder"
         :reducer="lookaheadReducer"
         :data="entries"
