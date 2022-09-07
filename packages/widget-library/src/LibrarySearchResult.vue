ac5e1e68c531ac368fc85dbc4496dab078389793
Jacob Wegner <jwegner@eldarion.com>
2022-09-07 15:13:36 -0500

    First pass at adding a typeahead to LibraryWidget

---
+++ packages/widget-library/src/LibrarySearchResult.vue
@@ -0,0 +1,30 @@
+<template>
+  <p class="search-result" @click="handleClick">
+    {{ JSON.stringify(result) }}
+  </p>
+</template>
+
+<script>
+  export default {
+    name: 'LibrarySearchResult',
+    props: ['result'],
+    methods: {
+      handleClick() {
+        // TODO: Refactor using ReaderLink component
+        this.$router.push({
+          to: 'reader',
+          params: { urn: this.result.urn },
+          query: this.$route.query,
+        });
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .search-result {
+    font-family: monospace;
+    font-size: 0.8em;
+    word-wrap: break-word;
+  }
+</style>
