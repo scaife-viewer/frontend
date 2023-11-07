cc2a5b9289009ac4e18a6168a86776391c614dc1
Jake Wegner <jake.wegner@signumu.org>
2023-11-07 09:21:22 -0600

    Refactor legend using  SCHOLIA_KIND_CHOICES

--- packages/common/src/constants.js
+++ packages/common/src/constants.js
@@ -24,6 +24,14 @@ export const SCHOLIA_KIND_INTERLINEAR = 'scholia-kind-interlinear';
 export const SCHOLIA_KIND_INTERMARGINAL = 'scholia-kind-intermarginal';
 export const SCHOLIA_KIND_INTERIOR = 'scholia-kind-interior';
 export const SCHOLIA_KIND_MISC = 'scholia-kind-misc';
+export const SCHOLIA_KIND_CHOICES = new Map([
+  [SCHOLIA_KIND_MAIN, 'Main'],
+  [SCHOLIA_KIND_EXTERIOR, 'Exterior'],
+  [SCHOLIA_KIND_INTERLINEAR, 'Interlinear'],
+  [SCHOLIA_KIND_INTERMARGINAL, 'Intermarginal'],
+  [SCHOLIA_KIND_INTERIOR, 'Interior'],
+  [SCHOLIA_KIND_MISC, 'Miscellaneous'],
+])
 
 export const SCHOLIA_URN_MAIN = 'urn:cts:greekLit:tlg5026.msA.hmt:';
 export const SCHOLIA_URN_EXTERIOR = 'urn:cts:greekLit:tlg5026.msAext.hmt:';
